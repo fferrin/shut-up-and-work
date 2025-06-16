87688bf410b56c1a5f839732c651fedf71fba5ef
Facundo Ferrin <facundo.ferrin@gmail.com>
2025-06-16 16:50:23 +0200

    Add functions in storage.js file

--- extension/storage.js
+++ extension/storage.js
@@ -1,22 +1,23 @@
 
 function saveSettings(hoursWorked, monthlySalary, showAsTime) {
-    chrome.storage.sync.set({
-        hoursWorked: hoursWorked,
-        monthlySalary: monthlySalary,
-        showAsTime: showAsTime,
+    chrome.storage.local.set({
+        hoursWorked,
+        monthlySalary,
+        hourlyRate: monthlySalary / hoursWorked,
+        showAsTime,
     });
 }
 
 function loadSettings() {
     return new Promise((resolve) => {
-        chrome.storage.sync.get(
-            ["hoursWorked", "monthlySalary", "showAsTime"],
+        chrome.storage.local.get(
+            ["hoursWorked", "monthlySalary", "hourlyRate", "showAsTime"],
             function (result) {
                 resolve({
-                    hoursWorked: result.hoursWorked || "",
-                    monthlySalary: result.monthlySalary || "",
-                    showAsTime: result.showAsTime !== undefined ? result.showAsTime : false,
-                });
+                    hoursWorked: result.hoursWorked,
+                    monthlySalary: result.monthlySalary,
+                    hourlyRate: result.hourlyRate,
+    });
             },
         );
     });
