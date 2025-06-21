4addf8efa85cb89ed74bbfbc381d0e58c1f884ad
Facundo Ferrin <facundo.ferrin@gmail.com>
2025-06-21 16:38:37 +0200

    Add linter, formatter and TS support

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,11 +2,23 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production', // or 'development'
-    entry: path.resolve(__dirname, 'extension/src/content.js'),
-    output: {
-        filename: 'content.bundle.js',
-        path: path.resolve(__dirname, 'extension/dist'),
-    },
-    target: 'web'
+  mode: 'production',
+  entry: path.resolve(__dirname, 'extension/src/content.js'),
+  output: {
+    filename: 'content.bundle.js',
+    path: path.resolve(__dirname, 'extension/dist'),
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
